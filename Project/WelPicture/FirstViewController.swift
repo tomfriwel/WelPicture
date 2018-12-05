c6502da4a1828f816ca271b701258090d5436158
tomfriwel <976932447@qq.com>
2018-12-05 17:36:20 +0800

    init

---
+++ Project/WelPicture/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WelPicture
+//
+//  Created by tomfriwel on 2018/12/5.
+//  Copyright © 2018 tomfriwel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Project/WelPicture/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  WelPicture
+//
+//  Created by tomfriwel on 2018/12/5.
+//  Copyright © 2018 tomfriwel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
